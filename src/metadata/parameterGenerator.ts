eeedbaa3b3648923754ea121369ac82b6634af96
Konrad Koch <hello@creadicted.com>
2017-09-20 09:30:07 +0200

    Typo in error description
    Changed typo and description.

--- src/metadata/parameterGenerator.ts
+++ src/metadata/parameterGenerator.ts
@@ -212,7 +212,7 @@ export class ParameterGenerator {
             throw new InvalidParameterException(`Parameter '${parameterName}:${type}' can't be passed as a path parameter in '${this.getCurrentLocation()}'.`);
         }
         if ((!this.path.includes(`{${pathName}}`)) && (!this.path.includes(`:${pathName}`))) {
-            throw new Error(`Parameter '${parameterName}' can't macth in path: '${this.path}'`);
+            throw new Error(`Parameter '${parameterName}' does not match one in path: '${this.path}'`);
         }
 
         return {
